d8dc2b3fae5b40906585aef39139b7c1de5f51d5
jnutterdev <hello@jnutterdev.com>
2021-03-15 16:21:16 -0400

    Started using blueprints for views and models, but running into an error with the functions from the models.py file. need to resolve that

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+models = Blueprint('models', __name__)
+
+#  DATABASE FUNCTIONS
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', 
+        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post 
